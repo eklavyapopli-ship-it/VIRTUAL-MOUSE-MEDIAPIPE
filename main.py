a3a753976e202f5f5d8c9641d7b4e87bf4afb8cd
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2025-10-28 13:15:57 +0530

    landmarks update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import mediapipe as mp
 import cv2
 import math
 import pyautogui
+import autopy
 pyautogui.FAILSAFE=False
 # -----
 mp_drawing= mp.solutions.drawing_utils
@@ -9,7 +10,7 @@ mp_holistic = mp.solutions.holistic
 screen_w, screen_h = pyautogui.size()
 cap = cv2.VideoCapture(0)
 
-with mp_holistic.Holistic(static_image_mode=False,min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+with mp_holistic.Holistic(min_detection_confidence=0.1, min_tracking_confidence=0.1) as holistic:
     while cap.isOpened():
         ret, frame = cap.read()
         flipped=cv2.flip(frame,1)
@@ -17,32 +18,19 @@ with mp_holistic.Holistic(static_image_mode=False,min_detection_confidence=0.5, 
         results= holistic.process(img)
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         
-        if results.left_hand_landmarks:
         
-            index_finger = results.left_hand_landmarks.landmark[8]
-            thumbs = results.left_hand_landmarks.landmark[4]
-            element = results.left_hand_landmarks.landmark[5]
-            h, w, _ = flipped.shape
-            x2=element.x
-            x1=element.x
-            y2=thumbs.y
-            y1=thumbs.y
-            distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-           
-            cx, cy = int(index_finger.x * w), int(index_finger.y * h)
-            bx, by = int(thumbs.x * w), int(thumbs.y * h)
-            ax,ay=int(element.x * w), int(element.y * h)
-            cv2.circle(flipped, (cx, cy), 8, (0, 255, 0), cv2.FILLED)
-            cv2.circle(flipped, (bx, by), 8, (0, 255, 0), cv2.FILLED)
-            cv2.circle(flipped, (ax, ay), 8, (0, 255, 0), cv2.FILLED)
+        mp_drawing.draw_landmarks(flipped, results.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
+        if results.left_hand_landmarks:
             
             
+            index_finger_tip = results.left_hand_landmarks.landmark[8]
          
-            pyautogui.moveTo(index_finger.x*screen_w,index_finger.y*screen_h)
+            middle_finger_tip=results.left_hand_landmarks.landmark[12]
+
+         
+            pyautogui.moveTo(index_finger_tip.x*screen_w,index_finger_tip.y*screen_h)
         
 
-            # if distance<210 and distance<220:
-            #     pyautogui.click()
             
         cv2.imshow('Holistic', flipped)
         if cv2.waitKey(10) & 0xff == ord('q'):
